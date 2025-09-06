84833244db150cfb5b87e6e4900d0e67c2f68876
imaayushk <kumaraayush327@gmail.com>
2025-09-07 00:49:33 +0530

    updated frontend url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   async function reviewCode() {
     setLoading(true);
     try {
-      const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+      const response = await axios.post('https://codereviewer-1-geui.onrender.com', { code });
       setreview(response.data);
     } catch (error) {
       console.error("Error fetching review:", error);
